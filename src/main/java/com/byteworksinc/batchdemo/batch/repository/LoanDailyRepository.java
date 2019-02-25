177ed6a9f3a6ab857f0e6052e3cf7e4bbfd79754
kc-mitchell <steve.mitchell@mobilecapitalgroup.com>
2019-02-25 14:17:22 -0600

    Added JUnit test for BatchJobScheduler

--- src/main/java/com/byteworksinc/batchdemo/batch/repository/LoanDailyRepository.java
+++ src/main/java/com/byteworksinc/batchdemo/batch/repository/LoanDailyRepository.java
@@ -2,7 +2,15 @@ package com.byteworksinc.batchdemo.batch.repository;
 
 import com.byteworksinc.batchdemo.domain.LoanDaily;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface LoanDailyRepository extends JpaRepository<LoanDaily, String> {
+
+  @Modifying
+  @Transactional
+  @Query("delete from LoanDaily l")
+  void deleteAllWithQuery();
 
 }
--- src/test/java/com/byteworksinc/batchdemo/test/batch/BatchJobScheduleTest.java
+++ src/test/java/com/byteworksinc/batchdemo/test/batch/BatchJobScheduleTest.java
@@ -4,6 +4,7 @@ import static junit.framework.TestCase.assertNotNull;
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
+import com.byteworksinc.batchdemo.batch.job.BatchJobScheduler;
 import com.byteworksinc.batchdemo.batch.repository.LoanDailyRepository;
 import com.byteworksinc.batchdemo.batch.repository.LoanRepository;
 import com.byteworksinc.batchdemo.domain.Loan;
@@ -12,9 +13,10 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
@@ -24,13 +26,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @ComponentScan("com.byteworksinc.batchdemo.test")
-public class DailyLoanBalanceTest {
+public class BatchJobScheduleTest {
 
   @Autowired
-  private JobLauncherTestUtils jobLauncherTestUtils;
+  private BatchJobScheduler batchJobScheduler;
 
   @Autowired
   private LoanRepository loanRepository;
+
 
   @Autowired
   private LoanDailyRepository loanDailyRepository;
@@ -42,15 +45,9 @@ public class DailyLoanBalanceTest {
   }
 
   @Test
-  public void launchJob() throws Exception {
-
-    //testing a job
-    JobExecution jobExecution = jobLauncherTestUtils.launchJob();
-
-    //Testing a individual step
-    //JobExecution jobExecution = jobLauncherTestUtils.launchStep("step1");
-
-    assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
+  public void testBatchJobScheduler()
+      throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
+    batchJobScheduler.scheduleTaskWithCronExpression();
 
     List<Loan> loans = loanRepository.findAll();
     List<LoanDaily> loanDailies = loanDailyRepository.findAll();
